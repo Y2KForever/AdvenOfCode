d28dc56aef31e84502affecf442fe7655e6569e4
Y2KForever <niklas.hansen1@gmail.com>
2021-12-06 13:23:11 +0100

    Day 6 - I hate these fishes

---
+++ 2021/6/js/6b.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+const fishes = fs
+  .readFileSync("./input.txt")
+  .toString()
+  .trim()
+  .split(",")
+  .map((item) => parseInt(item));
+const days = 256;
+let arrayOfFishes = new Array(9).fill(0);
+fishes.forEach((time) => arrayOfFishes[time]++);
+
+for (let i = 0; i < days; i++) {
+  const resetFishes = arrayOfFishes.shift();
+  arrayOfFishes = [...arrayOfFishes, resetFishes];
+  arrayOfFishes[6] += resetFishes;
+}
+let answer = arrayOfFishes.reduce((acc, val) => acc + val, 0);
+
+console.log(answer);
