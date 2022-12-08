d3f718ce47d21a5d4f21cdf3d92683b86bc60ee8
Y2KForever <niklas.hansen1@gmail.com>
2022-12-08 17:02:39 +0100

    trimEnd to readInput

--- 2022/utils/index.js
+++ 2022/utils/index.js
@@ -1,9 +1,11 @@
 const readInput = (input) => {
   const fs = require("fs");
-  const file = fs.readFileSync(input, {
-    flag: "r",
-    encoding: "utf-8",
-  });
+  const file = fs
+    .readFileSync(input, {
+      flag: "r",
+      encoding: "utf-8",
+    })
+    .trimEnd();
   return file.split(process.platform === "win32" ? "\r\n" : "\n");
 };
 
