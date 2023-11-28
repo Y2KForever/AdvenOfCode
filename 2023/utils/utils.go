1be91213d92f192700175f4268c6f98fa0ed5ca4
Y2KForever <niklas.hansen1@gmail.com>
2023-11-28 12:28:13 +0100

    ReadInput util

--- 2023/utils/utils.go
+++ 2023/utils/utils.go
@@ -1,1 +1,22 @@
 package utils
+
+import (
+	"os"
+	"runtime"
+	"strings"
+)
+
+func ReadInput(input string) ([]string, error) {
+	fileBytes, err := os.ReadFile(input)
+	if err != nil {
+		return nil, err
+	}
+	fileContent := strings.TrimRight(string(fileBytes), "\r\n")
+	lineBreak := "\r\n"
+
+	if runtime.GOOS != "windows" {
+		lineBreak = "\n"
+	}
+	lines := strings.Split(fileContent, lineBreak)
+	return lines, nil
+}
