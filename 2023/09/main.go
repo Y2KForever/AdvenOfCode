c5ab79e97b3e2fd6936991da7d72e7c772aea34f
Y2KForever <niklas.hansen1@gmail.com>
2023-12-09 15:45:30 +0100

    Day 09 2023

---
+++ 2023/09/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"aoc-2023/utils"
+	"encoding/json"
+	"fmt"
+	"slices"
+	"strings"
+)
+
+func main() {
+	input, _ := utils.ReadInput("input.txt")
+
+	var next func([]int) int
+	next = func(s []int) int {
+		h := []int{}
+		for i := 0; i < len(s)-1; i++ {
+			h = append(h, s[i+1]-s[i])
+		}
+		if !slices.ContainsFunc(h, func(x int) bool { return x != 0 }) {
+			return s[len(s)-1]
+		}
+		return s[len(s)-1] + next(h)
+	}
+
+	part1, part2 := 0, 0
+	for _, s := range input {
+		var seq []int
+		json.Unmarshal([]byte("["+strings.ReplaceAll(s, " ", ",")+"]"), &seq)
+
+		part1 += next(seq)
+		slices.Reverse(seq)
+		part2 += next(seq)
+	}
+	fmt.Println(part1)
+	fmt.Println(part2)
+}
